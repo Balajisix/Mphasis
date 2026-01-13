d6bf458e65ded4972c1eece2e5899217ca56c89b
Balaji V4 <balaji.v4@mphasis.com>
2026-01-13 15:35:17 +0530

    memory maintained

--- llm.py
+++ llm.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.messages import HumanMessage, AIMessage
 import streamlit as st
 
 model_id = "openai/gpt-oss-120b"
@@ -12,16 +13,32 @@ llm_endpoint  = HuggingFaceEndpoint(
 llmhg = ChatHuggingFace(llm=llm_endpoint)
 
 st.title("BalaGPT")
-st.write("Please enter your prompt")
-prompt = st.text_area("Prompt:", "")
-if st.button("Generate Response"):
-    if prompt.strip() == "":
-        st.error("Please enter your prompt first")
-    else:
-        response = llmhg.invoke(prompt)
-        text_output = response.content
-        st.success("Response Generated")
-        st.write(response.content)
 
-# formatted = text_output.replace("\n", " ")
-# print(formatted)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for msg in st.session_state.messages:
+    role = "user" if isinstance(msg, HumanMessage) else "assistant"
+    with st.chat_message(role):
+        st.markdown(msg.content)
+
+if prompt := st.chat_input("Prompt"):
+    st.session_state.messages.append(HumanMessage(content=prompt))
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    res = llmhg.invoke(st.session_state.messages)
+    st.session_state.messages.append(AIMessage(content=res.content))
+    with st.chat_message("assistant"):
+        st.markdown(res.content)
+
+
+# st.write("Please enter your prompt")
+# prompt = st.text_area("Prompt:", "")
+# if st.button("Generate Response"):
+#     if prompt.strip() == "":
+#         st.error("Please enter your prompt first")
+#     else:
+#         response = llmhg.invoke(prompt)
+#         text_output = response.content
+#         st.success("Response Generated")
+#         st.write(response.content)
